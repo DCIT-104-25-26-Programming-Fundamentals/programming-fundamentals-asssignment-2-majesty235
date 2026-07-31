e57eebe3dfd1b88c86f843aedc94f7009f9f7dfb
majesty235 <eboatemaa012@st.ug.edu.gh>
2026-07-31 19:20:39 +0000

    implement Fibonacci sequence generator and membership check

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,65 @@
 #include <iostream>
 using namespace std;
 
+void printFibonacci(int n) {
+    if (n <= 0) {
+        cout << "Error: Number of terms must be positive." << endl;
+        return;
+    }
+
+    int a = 0, b = 1;
+
+    cout << "Fibonacci sequence: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << endl;
+}
+
+bool isFibonacci(int number) {
+    if (number < 0) {
+        return false;
+    }
+
+    int a = 0, b = 1;
+
+    if (number == a) {
+        return true;
+    }
+
+    while (a <= number) {
+        if (a == number) {
+            return true;
+        }
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return false;
+}
+
+int main() {
+    int n;
+
+    cout << "How many terms? ";
+    cin >> n;
+    printFibonacci(n);
+
+    cout << endl;
+
+    int number;
+    cout << "Enter a number to check: ";
+    cin >> number;
+
+    if (isFibonacci(number)) {
+        cout << number << " is a Fibonacci number." << endl;
+    } else {
+        cout << number << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
